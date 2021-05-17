bfc8509c43aa287b5892a557196fe79c2e5abc33
wejdeD1 <wejderd1@gmail.com>
2021-05-17 12:40:19 +0200

    Updates

---
+++ precache-manifest.3f2cf01139779eb07aa9384d606ff419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9d035f5e94daf3724539a285b2dc46",
+    "url": "/resto/index.html"
+  },
+  {
+    "revision": "d46afce4b607d2c0fd63",
+    "url": "/resto/static/css/main.5d9ed5ef.chunk.css"
+  },
+  {
+    "revision": "6cc9540664a807f0a261",
+    "url": "/resto/static/js/2.963f1a57.chunk.js"
+  },
+  {
+    "revision": "d46afce4b607d2c0fd63",
+    "url": "/resto/static/js/main.8a42ad8f.chunk.js"
+  },
+  {
+    "revision": "24cb0de82746be0650fc",
+    "url": "/resto/static/js/runtime~main.ecd9c60b.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/resto/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/resto/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
